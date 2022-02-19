9d2f66b633c67c0eefb9a3ea94530d9380c5162f
gp1108 <pietrogirotto00@gmail.com>
2022-02-19 11:43:54 +0100

    init

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('First commit')
+require('dotenv').config();
+require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+const express = require('express');
+const app = express();
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+const port = process.env.PORT || 5000
