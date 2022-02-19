6cff1877748b42abdd46870db39e0b96593a07d8
gp1108 <pietrogirotto00@gmail.com>
2022-02-19 12:32:13 +0100

    added connection

---
+++ db/connectDB.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+function connectDB(url) {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+module.exports = connectDB;
