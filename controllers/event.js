8ff9ffa33f71b702414b1401a7e95536ff48d650
gp1108 <pietrogirotto00@gmail.com>
2022-02-19 15:44:18 +0100

    main functionallity createEvent

--- controllers/event.js
+++ controllers/event.js
@@ -1,5 +1,20 @@
+const {StatusCodes} = require('http-status-codes');
+const {BadResquestError} = require('../errors');
+const Event = require('../models/Event.js');
+
 async function createEvent(req, res) {
-  res.send('create event');
+  const {
+    body: {days, name},
+  } = req;
+  if (!days || !name) {
+    throw new BadResquestError('Please provide both name and dates');
+  }
+  const dates = days.split(',');
+  const event = await Event.create({
+    name,
+    days: dates,
+  });
+  res.status(StatusCodes.CREATED).json(event);
 }
 
 async function getEvent(req, res) {
